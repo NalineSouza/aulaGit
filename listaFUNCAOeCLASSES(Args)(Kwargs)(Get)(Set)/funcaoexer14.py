98ac9eed4df9b18ad9d00751f94fe1077a0d6cf0
Naline Souza <143421320+NalineSouza@users.noreply.github.com>
2024-08-14 11:06:05 -0400

    funcao consumo eletrico
    funcao consumo eletrico

--- listaFUNCAOeCLASSES(Args)(Kwargs)(Get)(Set)/funcaoexer14.py
+++ listaFUNCAOeCLASSES(Args)(Kwargs)(Get)(Set)/funcaoexer14.py
@@ -3,10 +3,11 @@ def consumo_luz(potencia_eletrica,tempo_ligado):
     return resultado
 
 
-
+def calcular_conta_energia(consumo,valorkwh):
+    return consumo * valorkwh
 
 tempoligado = float(input("Digite o tempo em que o aparelho ficou ligado: "))
 potenciaeletrica = float(input("Digite  a potencia por hora:  "))
 
 res = consumo_luz(tempoligado,potenciaeletrica)
-print(res)
+ress = calcular_conta_energia()
